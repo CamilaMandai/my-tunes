bce2c4bb39c1dd5aa132c018e8764b77697e4d62
Camila Mandai <cayumandai@gmail.com>
2022-08-30 15:01:41 -0300

    [Camila Mandai] Project Trybetunes

---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        <p>404 - Page not found</p>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
---
+++ src/pages/ProfileEdit.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProfileEdit extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">
+        <p>Profile Edit</p>
+      </div>
+    );
+  }
+}
+
+export default ProfileEdit;
