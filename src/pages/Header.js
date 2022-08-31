18e3ea0a9e25817ad31ee3fabdc3fb018f00539d
Camila Mandai <cayumandai@gmail.com>
2022-08-31 09:43:37 -0300

    implementa requisito 4

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 class Header extends React.Component {
@@ -20,10 +21,26 @@ class Header extends React.Component {
   render() {
     //   const {loading} = this.state;
     const { user, loading } = this.state;
+    const headerComponent = (
+      <div>
+        <p data-testid="header-user-name">{user.name}</p>
+        <ul>
+          <Link to="/search">
+            <li data-testid="link-to-search"> Pesquisar </li>
+          </Link>
+          <Link to="/favorites">
+            <li data-testid="link-to-favorites"> Favoritos </li>
+          </Link>
+          <Link to="/profile">
+            <li data-testid="link-to-profile"> Perfil </li>
+          </Link>
+        </ul>
+      </div>
+    );
     return (
       <div data-testid="header-component">
-        { loading ? <p>Carregando...</p>
-          : <p data-testid="header-user-name">{user.name}</p>}
+        {loading ? <p>Carregando...</p>
+          : headerComponent}
         {/* <p>{user.name}</p> */}
       </div>
     );
