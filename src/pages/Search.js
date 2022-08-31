9134020d7dedfac1fd4792d902901c7dabf5e850
Camila Mandai <cayumandai@gmail.com>
2022-08-31 09:57:50 -0300

    implementa requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,15 +2,38 @@ import React from 'react';
 import Header from './Header';
 
 class Search extends React.Component {
+  state = {
+    disabledBtn: true,
+  };
+
+  checkInputLength = ({ target }) => {
+    const { value } = target;
+    const MIN_SIZE = 2;
+    this.setState({ disabledBtn: (value.length < MIN_SIZE) });
+  };
+
   render() {
-    // const { loadingUser } = this.props;
-    // if (loadingUser) {
-    //   return (<div>Carregando</div>);
-    // }
+    const { disabledBtn } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Search</p>
+        <p>Pesquisar</p>
+        <form>
+          <input
+            name="artistName"
+            type="text"
+            data-testid="search-artist-input"
+            placeholder="Nome do artista"
+            onChange={ this.checkInputLength }
+          />
+        </form>
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ disabledBtn }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
